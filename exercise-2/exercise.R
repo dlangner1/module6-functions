056a8b9f92a5fa989a034610b1d430a83dc71043
Dustin Langner <dustin.langner1@gmail.com>
2017-01-10 19:25:37 -0800

    complete exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,13 +4,34 @@
 # the sentence of the form
 #   "The difference in length is N"
 
+  CompareLength <- function(string1, string2) {
+    diff.in.length <- abs(nchar(string1) - nchar(string2))
+    result <- paste("The difference in length is", diff.in.length)
+    return(result)
+  }
+
 
 # Pass two strings of different lengths to your `CompareLength` function
+  
+  difference <- CompareLength("Dustin", "Langner")
 
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
+  
+  DescribeDifference <- function(string1, string2) {
+    diff <- nchar(string1) - nchar(string2)
+    longer.string <- ""
+    if (diff > 0) {
+      longer.string <- "first"
+    } else {
+      longer.string <- "second"
+    }
+    return(paste("Your ", longer.string, " is longer by ", abs(diff), " characters" ))
+  }
 
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+  
+  longest.string <- DescribeDifference("Dustin", "Langner")
